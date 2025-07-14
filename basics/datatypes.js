409bd475576b07e9b7d1c51026f6eb3550871fdd
Pavan Archer <88071132+Pavan-Archer@users.noreply.github.com>
2025-07-14 22:05:52 +0000

    variables and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+'use strict'; // treat all JS code as new version
+
+let name ='pavan'
+let age = 27
+
+//number => 2 to power 28
+// bigint
+// string => ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+// console.log(typeof name,age);
---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = 'pavanarcher18@gmail.com'
+var accountPassword = '12345'
+accountCity = 'Bangalore'
+
+// accountId = 281297
+
+accountEmail = 'lakku@gmail.com'
+
+console.table([accountId, accountEmail,accountPassword, accountCity]);
