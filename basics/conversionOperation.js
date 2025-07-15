f615ca220e1574a43d089ae7ea97c31446b44deb
Pavan Archer <88071132+Pavan-Archer@users.noreply.github.com>
2025-07-15 00:03:12 +0000

    conversons operations commit

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -2,4 +2,40 @@ let number = 28
 
 let convertNum = Number(number)
 
-console.log(typeof (convertNum));
+// console.log(typeof (convertNum));
+
+// *********** Operations *********** 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); 
+
+// type of => Result 
+// undefined = undefined
+// null = object
+// boolean = boolean
+// number = number
+// string = string
+
+const id = 123
+const val = undefined
+const nullVal = null
+
+const heros = ['ironman, spiderman, loki']
+const myObj = {
+name : "spiderman",
+city : "queens"
+}
+
+// console.log(typeof id);
+// console.log(typeof val);
+// console.log(typeof nullVal);
+console.log(typeof heros);
+console.log(typeof myObj);
+
+
+
+
+
